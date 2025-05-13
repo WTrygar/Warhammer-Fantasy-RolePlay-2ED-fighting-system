ab0f8403477907464adc829f2393a81eb0465e61
WTrygar <chandelier.wt@gmail.com>
2025-05-13 19:04:41 +0200

    feat: updated game loop

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # imports
 import os
 
-from character import Enemy, Hero
+from character import Enemy, Hero, enemies
 from weapon import iron_sword, short_bow
 
 # setup
@@ -9,15 +9,43 @@ hero = Hero(name="Hero", health=100)
 hero.equip(iron_sword)
 enemy = Enemy(name="enemy", health=100, weapon=short_bow)
 
+# game class
+class Game:
+  def __init__(self):
+    self.running = True
+
+  @staticmethod
+  def spawn_enemy() -> Enemy:
+    if enemies:
+      enemy = enemies[0]
+      input(f"Spawning {enemy.name}... ")
+      return enemy
+    else:
+      input("YOU WIN")
+      exit()
+
+  def run(self):
+    enemy = self.spawn_enemy()
+    while self.running:
+      os.system("cls")
+
+      hero.attack(enemy)
+      enemy.attack(hero)
+
+      hero.health_bar.draw()
+      enemy.health_bar.draw()
+
+      input()
+
+      self.running = hero.health > 0
+
+      if enemy.health <= 0:
+        enemies.pop(0)
+        enemy = self.spawn_enemy()
+    input("GAME OVER")
+    exit()
+
 # game loop
-while True:
-  os.system("cls")
-  hero.attack(enemy)
-  enemy.attack(hero)
-
-  hero.health_bar.draw()
-  enemy.health_bar.draw()
-
-  # hero.drop()
-  
-  input()
+if __name__ == "__main__":
+  game = Game()
+  game.run
