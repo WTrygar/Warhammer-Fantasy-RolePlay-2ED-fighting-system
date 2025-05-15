79ddacf8f2cfe0f9375cc17c2e5aeb0723cba0da
WTrygar <chandelier.wt@gmail.com>
2025-05-15 16:05:13 +0200

    feat: added WFRP 2ED data for weapons: class Weapon, MeleeWeapon, MissileWeapon and some dummy data for them.

---
+++ warhammer_weapons.py
@@ -0,0 +1,69 @@
+class Weapon:
+  def __init__(self,
+               name: str,
+               cost: int,
+               encumbrance: int,
+               group: str,
+               damage: int,
+               qualities: list,
+               availability: str):
+    self.name = name
+    self.cost = cost
+    self.encumbrance = encumbrance
+    self.group = group
+    self.damage = damage
+    self.qualities = qualities
+    self.availability = availability
+    
+  def __str__(self):
+    return f'{self.name}'
+
+class MeleeWeapon(Weapon):
+  def __init__(self, name, cost, encumbrance, group, damage, qualities, availability):
+    super().__init__(name, cost, encumbrance, group, damage, qualities, availability)
+
+
+class MissileWeapon(Weapon):
+  def __init__(self, name, cost, encumbrance, group, damage, qualities, availability, range: list, reload: int):
+    super().__init__(name, cost, encumbrance, group, damage, qualities, availability)
+    self.range = range
+    self.reload = reload
+
+
+fists = MeleeWeapon(name = "Fists",
+                    cost = 0,
+                    encumbrance = 0,
+                    group = "Ordinary",
+                    damage = 1,
+                    qualities = [],
+                    availability = "Common")
+
+hochlandLongRifle = MissileWeapon(name = "Hohland Long Rifle",
+                               cost = 450,
+                               encumbrance = 70,
+                               group = "Engineer",
+                               damage = 4,
+                               range = [48, 96],
+                               reload = 4,
+                               qualities = ["Impact", "Unreliable"],
+                               availability = "Very Rare")
+
+#TESTING
+if __name__ == "__main__":
+  print(f"Weapon name: {fists.name}")
+  print(f"Cost: {fists.cost}")
+  print(f"Encumbrance: {fists.encumbrance}")
+  print(f"Group: {fists.group}")
+  print(f"Damage: {fists.damage}")
+  print(f"Qualities: {fists.qualities}")
+  print(f"Availability: {fists.availability}")
+  print(f" ")
+  print(f"Weapon name: {hochlandLongRifle.name}")
+  print(f"Cost: {hochlandLongRifle.cost}")
+  print(f"Encumbrance: {hochlandLongRifle.encumbrance}")
+  print(f"Group: {hochlandLongRifle.group}")
+  print(f"Damage: {hochlandLongRifle.damage}")
+  print(f"Range: {hochlandLongRifle.range}")
+  print(f"Reload: {hochlandLongRifle.reload}")
+  print(f"Qualities: {hochlandLongRifle.qualities}")
+  print(f"Availability: {hochlandLongRifle.availability}")
